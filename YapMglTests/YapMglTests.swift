49ef039e3b4f924e6f2289426788b122c65687d5
額尓敦宝力格 <eridenboriga@gakujiatsushitakararyokukaku-no-macbook-pro.local>
2017-11-04 19:43:43 +0900

    Initial Commit

---
+++ YapMglTests/YapMglTests.swift
@@ -0,0 +1,36 @@
+//
+//  YapMglTests.swift
+//  YapMglTests
+//
+//  Created by 額尓敦宝力格 on 2017/11/04.
+//  Copyright © 2017年 額尓敦宝力格. All rights reserved.
+//
+
+import XCTest
+@testable import YapMgl
+
+class YapMglTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
